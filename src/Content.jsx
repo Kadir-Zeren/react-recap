7650359f4dd986eca3bf1b29996bc726c4e26f39
kadir-zeren <k4dzeren@gmail.com>
2023-11-14 18:04:48 +0300

    img

--- src/Content.jsx
+++ src/Content.jsx
@@ -4,10 +4,27 @@ const Content = () => {
     fontSize: "1.1rem",
     textAlign: "justify",
   };
+  const imgStyle = {
+    borderRadius: "20px",
+    width: "400px",
+    cursor: "pointer",
+    display: "block",
+    margin: "auto",
+  };
   return (
     <main>
       <h2 style={{ color: "red", backgroundColor: "black" }}>React Recap</h2>
       <p>react js </p>
+      <img
+        style={imgStyle}
+        src="https://cdn.pixabay.com/photo/2023/10/26/18/18/coneflower-8343278_640.jpg"
+        alt=""
+      />
+      <img
+        style={imgStyle}
+        src="https://cdn.pixabay.com/photo/2023/10/01/13/54/bird-8287451_1280.jpg"
+        alt=""
+      />
       <p style={parStyle}>
         Lorem ipsum dolor sit, amet consectetur adipisicing.
       </p>
