b268bd18837a045c9b6de4a763f266be349e4ac1
kadir-zeren <k4dzeren@gmail.com>
2023-11-14 17:55:35 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Content from "./Content";
+import { Footer } from "./Footer";
+import Header from "./Header";
+
+console.log("deneme");
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Content.jsx
@@ -0,0 +1,14 @@
+const Content = () => {
+    const parStyle={
+        fontFamily:"tahoma"
+    }
+  return (
+    <main>
+      <h2 style={{ color: "red", backgroundColor: "black" }}>React Recap</h2>
+      <p>react js </p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+    </main>
+  );
+};
+
+export default Content;
