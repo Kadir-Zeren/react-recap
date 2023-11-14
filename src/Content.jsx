fc6915af537b67ae6c76924c7a7735c5165aef90
kadir-zeren <k4dzeren@gmail.com>
2023-11-14 17:57:43 +0300

    react-recap

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,12 +1,16 @@
 const Content = () => {
-    const parStyle={
-        fontFamily:"tahoma"
-    }
+  const parStyle = {
+    fontFamily: "tahoma",
+    fontSize: "1.1rem",
+    textAlign: "justify",
+  };
   return (
     <main>
       <h2 style={{ color: "red", backgroundColor: "black" }}>React Recap</h2>
       <p>react js </p>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+      <p style={parStyle}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing.
+      </p>
     </main>
   );
 };
